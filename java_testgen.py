f0cb65f522a24a567da7f5da32c0dc0e7a6b293e
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-04-20 18:04:45 +0200

    fun with jvm benchmarks

--- java_testgen.py
+++ java_testgen.py
@@ -1,5 +1,6 @@
 import subprocess
 import random
+import os
 
 
 def generate(inpath, paramset):
@@ -16,22 +17,36 @@ def writejava(outpath, content):
         outputfile.write(content)
 
 
+def compilejava(outpath):
+    os.system('javac {}'.format(outpath))
+
+
 def get_jvm_exectime(prog, paramset):
     templatepath = './javasw/{}.jtmp'.format(prog)
     outpath = './javasw/{}.java'.format(prog)
+
+    compilejava(outpath)
     
     writejava(outpath, generate(templatepath, paramset))
 
-    proc = subprocess.Popen(['java', '-Xint', '-XX:-TieredCompilation', './javasw/{}.java'.format(prog)], stdout=subprocess.PIPE, text=True)
+    os.chdir('./javasw')
 
-    try:
-        out, _ = proc.communicate(timeout=30)
-    except TimeoutExpired:
-        proc.kill()
-        out, _ = proc.communicate()
-        return -1
+    timelist = []
+    for _ in range(100):
+        proc = subprocess.run(['perf', 'stat', '--event=cycles', 'java', '-Xint', prog], capture_output=True)
+        cycles = parse_cycles(proc.stderr.decode('utf-8'))
+        print(cycles)
+        timelist.append(cycles)
 
-    return int(out)
+    os.chdir('..')
+
+    return sum(timelist) / len(timelist)
+
+
+def parse_cycles(perfout):
+    for line in perfout.splitlines():
+        if 'cycles' in line:
+            return int(line.split()[0].replace(',', ''))
 
 
 def gen_random_array(length):
@@ -42,23 +57,26 @@ def gen_random_array(length):
 
 
 def intrev_jvmtime(i):
-    return get_jvm_exectime('IntReverse', { '$REVERSE_INPUT$': str(i) })
+    return get_jvm_exectime('IntReverse', { '$REVERSE_INPUT$': str(i), '$LOOPCOUNT$': '2000' })
 
 
 def sieve_jvmtime(i):
-    return get_jvm_exectime('PrimeSieve', { '$SIEVE_INPUT$': str(i) })
+    return get_jvm_exectime('PrimeSieve', { '$SIEVE_INPUT$': str(i), '$LOOPCOUNT$': '2000' })
 
 
 def towers_jvmtime(i):
-    return get_jvm_exectime('TowersOfHanoi', { '$TOWERS_INPUT$': str(i) })
+    return get_jvm_exectime('TowersOfHanoi', { '$TOWERS_INPUT$': str(i), '$LOOPCOUNT$': '2000' })
 
 
 def recursive_jvmtime(i):
-    return get_jvm_exectime('RecursiveMath', { '$MATH_OP_1$': str(i), '$MATH_OP_2$': str(i) })
+    return get_jvm_exectime('RecursiveMath', { '$MATH_OP_1$': str(i), '$MATH_OP_2$': str(i), '$LOOPCOUNT$': '2000' })
 
 
 def qsort_jvmtime(i):
-    return get_jvm_exectime('QuickSort', { '$QSORT_INPUT_ARRAY$': gen_random_array(i), '$QSORT_INPUT_LEN$': str(i - 1) })
+    return get_jvm_exectime('QuickSort', { '$QSORT_INPUT_ARRAY$': gen_random_array(i), '$QSORT_INPUT_LEN$': str(i - 1), '$LOOPCOUNT$': '2000' })
+
+def test_jvmtime():
+    return get_jvm_exectime('Test', { })
 
 
 random.seed(1)
@@ -67,7 +85,7 @@ intrev = [1, 12, 123, 1234, 12345]
 sieve = [10, 20, 50, 100, 150, 200]
 towers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 recursive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-qsort = [10, 20, 50, 100, 150, 200]
+qsort = [10, 20, 50, 100]
 
 csvoutput = []
 csvoutput.append('program;input;exectime\n')
@@ -82,6 +100,8 @@ for i in recursive:
     csvoutput.append('{};{};{}\n'.format('RecursiveMath', i, recursive_jvmtime(i)))
 for i in qsort:
     csvoutput.append('{};{};{}\n'.format('QuickSort', i, qsort_jvmtime(i)))
+
+csvoutput.append('{};{};{}\n'.format('Test', 0, test_jvmtime()))
 
 with open('jvmexectime.csv', 'w') as csvout:
     csvout.writelines(csvoutput)
