0cbebe822d518ed5814828b4d825b9482f3dea8a
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-04-21 22:08:07 +0200

    visualizations work

--- exectime_graph.py
+++ exectime_graph.py
@@ -1,5 +1,30 @@
 from matplotlib import pyplot as plt
 import csv
+import math
+
+
+def read_jvm_results(filename):
+    results = {}
+    with open(filename, 'r') as resfile:
+        rdr = csv.reader(resfile, delimiter=';')
+        next(rdr, None)
+        baseline = math.floor(float(next(rdr, None)[3]))
+        for row in rdr:
+            progname = row[0]
+            inputval = int(row[1])
+            loopcount = int(row[2])
+            exectime = math.floor(float(row[3]))
+            if progname in results:
+                if loopcount in results[progname]:
+                    results[progname][loopcount].append((inputval, exectime - baseline))
+                else:
+                    results[progname][loopcount] = []
+                    results[progname][loopcount].append((inputval, exectime - baseline))
+            else:
+                results[progname] = {}
+                results[progname][loopcount] = []
+                results[progname][loopcount].append((inputval, exectime - baseline))
+    return results
 
 
 def read_results(filename):
@@ -10,26 +35,33 @@ def read_results(filename):
         for row in rdr:
             progname = row[0]
             inputval = int(row[1])
-            exectime = int(row[2])
+            loopcount = int(row[2])
+            exectime = math.floor(float(row[3]))
             if progname in results:
-                results[progname].append((inputval, exectime))
+                if loopcount in results[progname]:
+                    results[progname][loopcount].append((inputval, exectime))
+                else:
+                    results[progname][loopcount] = []
+                    results[progname][loopcount].append((inputval, exectime))
             else:
-                results[progname] = []
-                results[progname].append((inputval, exectime))
+                results[progname] = {}
+                results[progname][loopcount] = []
+                results[progname][loopcount].append((inputval, exectime))
     return results
 
 
 balidata = read_results('exectime.csv')
-jvmdata = read_results('jvmexectime.csv')
+jvmdata = read_jvm_results('jvmexectime.csv')
 prognames = ['IntReverse', 'TowersOfHanoi', 'QuickSort', 'RecursiveMath', 'PrimeSieve']
 
 for progname in prognames:
-    x = [i[0] for i in balidata[progname]]
-    bali_y = [i[1] * 10 for i in balidata[progname]]
-    jvm_y = [i[1] for i in jvmdata[progname]]
-    plt.scatter(x, bali_y, c='b', marker='o', label='Bali')
-    plt.scatter(x, jvm_y, c='r', marker='o', label='JVM')
-    plt.yscale('log')
-    plt.legend(loc='upper left')
-    plt.title(progname)
-    plt.show()
+    for loopcount in balidata[progname]:
+        x = [i[0] for i in balidata[progname][loopcount]]
+        bali_y = [i[1] for i in balidata[progname][loopcount]]
+        jvm_y = [i[1] for i in jvmdata[progname][loopcount]]
+        plt.scatter(x, bali_y, c='b', marker='o', label='Bali')
+        plt.scatter(x, jvm_y, c='r', marker='o', label='JVM')
+        plt.yscale('log')
+        plt.legend(loc='upper left')
+        plt.title('{} with {} Loops'.format(progname, loopcount))
+        plt.show()
